a1ebb7ee94fc1fb2dc18fd330064312c538b3353
GustavoMenezes33 <106703718+GustavoMenezes33@users.noreply.github.com>
2024-09-09 17:49:50 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,21 @@
+let dados = [
+        freddie = {
+        titulo : "Freddie Mercury",
+        descricao : "Considerado um dos maiores vocalistas de todos os tempos, conhecido por sua voz poderosa e performances exuberantes.",
+        link : "https://en.wikipedia.org/wiki/Freddie_Mercury",
+        tags : "rock heavy metal vocal voz"
+    },
+        robert = {
+        titulo : "Robert Plant",
+        descricao : "Lendário vocalista do Led Zeppelin, conhecido por sua voz poderosa e range vocal impressionante.",
+        link : "https://en.wikipedia.org/wiki/Robert_Plant",   
+        tags : "rock heavy metal vocal voz"
+    },
+        freddie = {
+        titulo : "Ozzy Osbourne",
+        descricao : "Considerado o Príncipe das Trevas, fundador do Black Sabbath e ícone do heavy metal.",
+        link : "https://en.wikipedia.org/wiki/Ozzy_Osbourne",    
+        tags : "rock heavy metal vocal voz"
+    }
+];
+
